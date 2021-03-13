a378535a029abb2abb9491aa38f1739b59fbbc6e
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-03-13 19:25:30 +0900

    Update loginValid.js

--- dockerExpress/controllers/loginValid.js
+++ dockerExpress/controllers/loginValid.js
@@ -1,22 +1,7 @@
-const express = require('express');
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
-const router = express.Router();
-const mysql = require('mysql2');
-const connection = mysql.createConnection({
-  user: 'root',
-  password: 'root',
-  database: 'my_mysql_db',
-  host: 'my_mysql'
-});
-
-connection.connect((err) => {
-    if (err) {
-        console.log('error connectiong: ' + err.stack);
-        return;
-    }
-    console.log('success');
-});
+const { Sequelize } = require('sequelize');
+const db = require('../models/DBconfig');
 
 exports.rootAccessControl = {
   validResult: (req, res) => {
@@ -28,35 +13,36 @@ exports.rootAccessControl = {
         err: loginErrorMessage1
       });
     } else {
-      const columns = ['name', 'email', 'password']
-      connection
-        .query(
-            'select ?? from ?? where email = ? and password = ?',
-            [columns, 'users', email, password],
-            (err, results, fields) => {
-                if (!results[0]) {
-                    const loginErrorMessage2 = 'メールアドレスまたはパスワードが間違っています';
-                    res.render('index', {
-                        err: loginErrorMessage2
-                    });
-                } else {
-                    const username = results[0].name;
-                    const queryEmail = results[0].email;
-                    const queryPassword = results[0].password;
-                    if (queryEmail === email && queryPassword === password) {
-                        const payload = {
-                            username: username
-                        };
-                        const token = jwt.sign(payload, config.jwt.secret, config.jwt.options);
-                        res.cookie('token', token, {maxAge: 3600000});
-                        res.redirect('/board');
-                    } else {
-                        const loginErrorMessage2 = 'メールアドレスまたはパスワードが間違っています';
-                        res.render('index', {
-                            err: loginErrorMessage2
-                        });
-                    }
-                } 
+      const columns = ['name', 'email', 'password'];
+      db.User.findOne({
+        where: {
+          email: email,
+          password: password
+        }
+      }).then(results => {
+        if (!results) {
+          const loginErrorMessage2 = 'メールアドレスまたはパスワードが間違っています';
+          res.render('index', {
+              err: loginErrorMessage2
+          });
+        } else {
+          const username = results.name;
+          const queryEmail = results.email;
+          const queryPassword = results.password;
+          if (queryEmail === email && queryPassword === password) {
+              const payload = {
+                  username: username
+              };
+              const token = jwt.sign(payload, config.jwt.secret, config.jwt.options);
+              res.cookie('token', token, {maxAge: 3600000});
+              res.redirect('/board');
+          } else {
+              const loginErrorMessage2 = 'メールアドレスまたはパスワードが間違っています';
+              res.render('index', {
+                  err: loginErrorMessage2
+              });
+          }
+        } 
       });
     }
   }
