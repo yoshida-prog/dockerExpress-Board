62a18bcbba50d189a644ce13455b57b9f7be9648
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-03-15 00:41:07 +0900

    Update logout.js

--- dockerExpress/controllers/logout.js
+++ dockerExpress/controllers/logout.js
@@ -3,7 +3,6 @@ const config = require('../config/config');
 
 module.exports = (req, res, next) => {
     const key = Object.keys(req.cookies);
-    console.log(key[0]);
     if (!key[0]) {
         res.status(401).json({
             message: 'You have not token'
