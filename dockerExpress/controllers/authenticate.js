2ee3547cc4880feb9a9a5284b88339eb2f0d88dc
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-03-22 04:46:51 +0900

    Update authenticate.js

--- dockerExpress/controllers/authenticate.js
+++ dockerExpress/controllers/authenticate.js
@@ -1,13 +1,14 @@
 const jwt = require('jsonwebtoken');
-const config = require('../config/config');
+const config = require('../config/jwtConfig');
 
 module.exports = (req, res, next) => {
     try {
         const token = req.cookies.token;
         const decoded = jwt.verify(token, config.jwt.secret);
         const username = decoded.username;
+        const url = req.url.replace('/', '');
         req.jwtPayload = decoded;
-        res.render('board', {username});
+        res.render(url, {username});
     } catch (err) {
         return res.status(401).json({
             message: 'Not authenticated'
