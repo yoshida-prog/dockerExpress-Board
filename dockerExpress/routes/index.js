d85e2224d46592fb2a1f6cf1ec08422374a2cf96
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-03-22 04:51:20 +0900

    Update index.js

--- dockerExpress/routes/index.js
+++ dockerExpress/routes/index.js
@@ -6,27 +6,30 @@ const { Sequelize } = require('sequelize');
 const db = require('../models/DBconfig');
 const registerValidController = require('../controllers/registerValid');
 const loginValidController = require('../controllers/loginValid');
+const createController = require('../controllers/createContent.js');
+const editContentController = require('../controllers/editContent');
+const deleteContentController = require('../controllers/deleteContent');
 const authenticate = require('../controllers/authenticate');
+const viewPostList = require('../controllers/viewPostList');
 const logout = require('../controllers/logout');
 const { body } = require('express-validator');
 const urlencodedParser = bodyParser.urlencoded({
   extended: false
 });
 
-/* GET home page. */
 router.get('/', async(req, res, next) => {
   res.render('index');
 });
-
 router.get('/register', (req, res) => {
   res.render('register');
 });
-
-router.get('/board', authenticate);
-
+router.get('/board', viewPostList);
+router.get('/create', authenticate);
 router.get('/logout', logout, (req, res) => {
   res.redirect('/');
 });
+router.get('/edit/:id', editContentController.rootAccessControl.editRouting);
+router.get('/delete/:id', deleteContentController.rootAccessControl.delete);
 
 router.post(
   '/register',
@@ -34,11 +37,20 @@ router.post(
   registerValidController.rootAccessControl.validCheck,
   registerValidController.rootAccessControl.validResult
 );
-
 router.post(
   '/index',
   urlencodedParser,
   loginValidController.rootAccessControl.validResult
+);
+router.post(
+  '/create',
+  urlencodedParser,
+  createController.rootAccessControl.createContent
+);
+router.post(
+  '/edit/:id',
+  urlencodedParser,
+  editContentController.rootAccessControl.editContent
 );
 
 module.exports = router;
